46be33ec419f52d0783bf803c509948e4a45db21
Nils Goecke <goeckenils@gmail.com>
2018-11-19 16:40:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#main-nav li a').click(function (e) {
+
+        var targetHref = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(targetHref).offset().top
+        }, 1000);
+
+        e.preventDefault();
+    });
+});
+$(document).ready(function () {
+    $('#btn a').click(function (e) {
+
+        var targetHref = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(targetHref).offset().top
+        }, 1000);
+
+        e.preventDefault();
+    });
+});
